8a3acb4a91016c10ae0893a46a84bc2957f9b11f
Andrew Cowper <andrew.cowper@slothrop.net>
2010-06-10 19:10:29 +0100

    started making timers

--- src/timers/core.clj
+++ src/timers/core.clj
@@ -1,16 +1,37 @@
 (ns timers.core
   (:use [clojure.contrib.duck-streams :as ds]
-        [clojure.contrib.server-socket :as ss]))
+        [clojure.contrib.server-socket :as ss])
+  (:import [java.io PushbackReader]))
+
+(def timers (atom {}))
+
+(defn make-timer [length]
+  (+ length (System/currentTimeMillis)))
+
+(defn new-timer [command]
+  (swap! timers assoc (:name command)
+         (make-timer (:length command))))
+
+(def commands {:new-timer new-timer})
+
+(defn dispatch [command]
+  (((:command command) commands) command))
 
 (defn do-stuff [in out]
-  (prn (.readLine (ds/reader in))))
+  (let [command (read (PushbackReader. (ds/reader in)))]
+    (dispatch command)))
 
 (defn main-loop []
   (ss/create-server 9899 do-stuff))
 
 (comment
   (def server (ss/create-server 9899 do-stuff))
-  (ss/close-server server))
+  (ss/close-server server)
+
+  ( (:command {:command :new-timer}) commands)
+
+  (:new-timer commands)
+  )
 
 
 
