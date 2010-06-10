ffa7d68639ba71397a8184af47d44a64080f3a7d
Andrew Cowper <andrew.cowper@slothrop.net>
2010-06-10 08:45:05 +0100

    socket connection - reading an input line

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject timers "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "timers for pomodoro"
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]])
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]])
--- src/timers/core.clj
+++ src/timers/core.clj
@@ -1,1 +1,16 @@
-(ns timers.core)
+(ns timers.core
+  (:use [clojure.contrib.duck-streams :as ds]
+        [clojure.contrib.server-socket :as ss]))
+
+(defn do-stuff [in out]
+  (prn (.readLine (ds/reader in))))
+
+(defn main-loop []
+  (ss/create-server 9899 do-stuff))
+
+(comment
+  (def server (ss/create-server 9899 do-stuff))
+  (ss/close-server server))
+
+
+
