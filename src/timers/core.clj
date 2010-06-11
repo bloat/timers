29734cb5b90a744bdb9431f1999d66b4ecd9cd74
Andrew Cowper <andrew.cowper@slothrop.net>
2010-06-11 08:13:41 +0100

    output, and time remaining

--- src/timers/core.clj
+++ src/timers/core.clj
@@ -1,7 +1,9 @@
 (ns timers.core
   (:use [clojure.contrib.duck-streams :as ds]
         [clojure.contrib.server-socket :as ss])
-  (:import [java.io PushbackReader]))
+  (:import [java.io PushbackReader]
+           [java.util Date]
+           [java.text SimpleDateFormat]))
 
 (def timers (atom {}))
 
@@ -10,28 +12,38 @@
 
 (defn new-timer [command]
   (swap! timers assoc (:name command)
-         (make-timer (:length command))))
+         (make-timer (:length command)))
+  (println @timers))
 
-(def commands {:new-timer new-timer})
+(defn millis-remaining [name]
+  (- (@timers name) (System/currentTimeMillis)))
+
+(defn time-left [command]
+  (println (.format (SimpleDateFormat. "mm:ss") (millis-remaining (:name command)))))
+
+(def commands {:new-timer new-timer :time-left time-left})
 
 (defn dispatch [command]
   (((:command command) commands) command))
 
 (defn do-stuff [in out]
   (let [command (read (PushbackReader. (ds/reader in)))]
-    (dispatch command)))
+    (binding [*out* (ds/writer out)]
+      (dispatch command)
+      (flush))))
 
 (defn main-loop []
   (ss/create-server 9899 do-stuff))
 
 (comment
-  (def server (ss/create-server 9899 do-stuff))
+  (def server (main-loop))
   (ss/close-server server)
-
+(* 1000 60 25)
   ( (:command {:command :new-timer}) commands)
 
   (:new-timer commands)
-  )
+
+)
 
 
 
